9d8d46314829879b05703f99ef6ec0ec327ad177
Reilly Forshaw <reilly.forshaw@gmail.com>
2020-12-01 09:25:22 -0800

    Generalize for part 2

--- 1-ReportRepair.swift
+++ 1-ReportRepair.swift
@@ -2,6 +2,18 @@
 
 import Foundation
 
+guard CommandLine.arguments.count == 3 else {
+    fatalError("Must provide target and number of records")
+}
+
+guard let target = Int(CommandLine.arguments[1]) else {
+    fatalError("Target must be an integer")
+}
+
+guard let numberOfRecords = Int(CommandLine.arguments[2]), numberOfRecords > 0 else {
+    fatalError("Number of records must be an integer greater than 0")
+}
+
 let root = URL(fileURLWithPath: (CommandLine.arguments[0] as NSString).deletingLastPathComponent)
 let inputURL = URL(fileURLWithPath: "1-ReportRepair-Input", relativeTo: root)
 
@@ -12,20 +24,44 @@ else {
     fatalError("Couldn't parse input")
 }
 
-var seen: Set<Int> = []
-var result: Int?
+/*
+ * Complexity:
+ * numberOfRecords == 1: O(n)
+ * numberOfRecords  > 1: O(n^(numberOfRecords - 1))
+ */
+func findRecords(summingTo target: Int, numberOfRecords: Int, in input: Array<Int>.SubSequence) -> [Int]? {
+    if numberOfRecords == 1 {
+        // Trivial case, for completeness
+        if input.contains(target) {
+            return [target]
+        } else {
+            return nil
+        }
+    } else if numberOfRecords == 2 {
+        // Base case
+        var seen: Set<Int> = []
 
-for record in input {
-    let other = 2020 - record
-    if seen.contains(other) {
-        result = record * (other)
-        break
+        for record in input {
+            let other = target - record
+            if seen.contains(other) {
+                return [other, record]
+            }
+            seen.insert(record)
+        }
+        return nil
+    } else {
+        // Recursion
+        for record in input {
+            if let result = findRecords(summingTo: target - record, numberOfRecords: numberOfRecords - 1, in: input.dropFirst()) {
+                return [record] + result
+            }
+        }
+        return nil
     }
-    seen.insert(record)
 }
 
-if let result = result {
-    print("Result: \(result)")
+if let result = findRecords(summingTo: target, numberOfRecords: numberOfRecords, in: input[...]) {
+    print("Result: \(result) => \(result.reduce(1, *))")
 } else {
     print("Result not found")
 }
